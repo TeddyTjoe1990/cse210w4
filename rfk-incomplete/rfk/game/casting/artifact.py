cf4be37602764fd5775cf04aa708777c2d9b4abe
TeddyTjoe1990 <her21059@byui.edu>
2022-10-01 15:11:59 +0700

    articat file was made but not finish yet

--- rfk-incomplete/rfk/game/casting/artifact.py
+++ rfk-incomplete/rfk/game/casting/artifact.py
@@ -1,3 +1,17 @@
 from game.casting.actor import Actor
 
 # TODO: Implement the Artifact class here. Don't forget to inherit from Actor!
+class Artifact(Actor):
+  """
+  An item of cultural or historical interest.
+  The responsibility of an Artifact is to provide a message about itself.
+  """
+  def __init__(self):
+    pass
+  
+  def get_message(self):
+    pass
+  
+  def set_message(self, message):
+    pass
+  
