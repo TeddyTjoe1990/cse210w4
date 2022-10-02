272284610f0fed61e1945f4209b83328ce45b36a
TeddyTjoe1990 <her21059@byui.edu>
2022-10-02 22:15:18 +0700

    artifact files has been done

--- rfk-incomplete/rfk/game/casting/artifact.py
+++ rfk-incomplete/rfk/game/casting/artifact.py
@@ -5,13 +5,18 @@ class Artifact(Actor):
   """
   An item of cultural or historical interest.
   The responsibility of an Artifact is to provide a message about itself.
+  Attributes:
+      _message (string): A short description about the artifact
   """
   def __init__(self):
-    pass
+    super().__init__()
+    self.message = ""
   
   def get_message(self):
-    pass
+    """Get the artifact's message"""
+    return self._message
   
   def set_message(self, message):
-    pass
+    """Updates the message to the given one"""
+    self._message = message
   
